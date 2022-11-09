48800a610437e33e2d5698f4944af70b1bd701cf
Chris Vouga <49178412+crvouga@users.noreply.github.com>
2022-11-08 17:55:25 -0700

    done

--- src/dynamodb-client.ts
+++ src/dynamodb-client.ts
@@ -2,7 +2,7 @@ import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
 import * as dotenv from "dotenv";
 dotenv.config();
 
-const REGION = "us-east-1";
+const REGION = "us-west-1";
 
 const accessKeyId = process.env["AWS_ACCESS_KEY_ID"];
 const secretAccessKey = process.env["AWS_SECRET_ACCESS_KEY"];
--- src/dynamodb-test.ts
+++ src/dynamodb-test.ts
@@ -1,45 +1,12 @@
-import { CreateTableCommand } from "@aws-sdk/client-dynamodb";
+import { ListTablesCommand } from "@aws-sdk/client-dynamodb";
 import { ddbClient } from "./dynamodb-client";
 
-const cmd = new CreateTableCommand({
-  AttributeDefinitions: [
-    {
-      AttributeName: "Season", //ATTRIBUTE_NAME_1
-      AttributeType: "N", //ATTRIBUTE_TYPE
-    },
-    {
-      AttributeName: "Episode", //ATTRIBUTE_NAME_2
-      AttributeType: "N", //ATTRIBUTE_TYPE
-    },
-  ],
-  KeySchema: [
-    {
-      AttributeName: "Season", //ATTRIBUTE_NAME_1
-      KeyType: "HASH",
-    },
-    {
-      AttributeName: "Episode", //ATTRIBUTE_NAME_2
-      KeyType: "RANGE",
-    },
-  ],
-  ProvisionedThroughput: {
-    ReadCapacityUnits: 1,
-    WriteCapacityUnits: 1,
-  },
-  TableName: "TEST_TABLE", //TABLE_NAME
-  StreamSpecification: {
-    StreamEnabled: false,
-  },
-});
-
-export const run = async () => {
+(async () => {
+  const command = new ListTablesCommand({});
   try {
-    const data = await ddbClient.send(cmd);
-    console.log("Table Created", data);
-    return data;
+    const results = await ddbClient.send(command);
+    console.log(results.TableNames?.join("/n"));
   } catch (err) {
-    console.log("Error", err);
+    console.error(err);
   }
-};
-
-run();
+})();
